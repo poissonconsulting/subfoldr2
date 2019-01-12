beeba3db486527b81d1183f49c90483c6f07a957
Joe Thorley <joe@poissonconsulting.ca>
2019-01-11 17:10:12 -0800

    fix sanitize_path

--- R/internal.R
+++ R/internal.R
@@ -15,7 +15,7 @@ file_path <- function(...) {
 sanitize_path <- function(path, rm_leading = TRUE) {
   path <- sub("//", "/", path)
   path <- sub("(.+)(/$)", "\\1", path)
-  if(isTRUE(rm_leading)) path <- sub("(^/)(.+)", "\\2", path)
+  if(isTRUE(rm_leading)) path <- sub("(^/)(.*)", "\\2", path)
   path
 }
 
