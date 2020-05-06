0833ed737d6634122c4460b5b4b6ea3de8e28dde
Joe Thorley <joe@poissonconsulting.ca>
2020-05-06 10:52:37 -0700

    - sbf_print only retries if grid errors

--- R/print.R
+++ R/print.R
@@ -1,23 +1,26 @@
-#' Print
+grid_errors <- function() {
+  c("cannot pop the top-level viewport", 
+    "no applicable method for 'depth'")
+}
+
+#' Print Object
 #'
-#' Tries to repeatedly print an object. 
-#' 
-#' This is a hack to solve the problem with ggplot2/grid graphics.
-#' 
+#' Retries printing an object if grid errors occurs.
+#'
 #' @param x An object to print.
-#' @param ntry A positive whole number specifying the number of attempts.
-#' @param silent A flag specifying whether to silence error messages.
+#' @param ntry A positive whole number specifying the number of tries.
 #'  
 #' @export
-sbf_print <- function(x, ntry = 3L, silent = FALSE) {
+sbf_print <- function(x, ntry = 3L) {
   chk_whole_number(ntry)
   chk_gt(ntry)
-  chk_flag(silent)
   i <- 1
   while(i < ntry) {
-    try <- try(print(x), silent = silent)
+    try <- try(print(x), silent = TRUE)
     if(!vld_is(try, "try-error"))
       return(invisible(try))
+    if(!any(grepl(grid_errors(), try)))
+      return(print(x))
     i <- i + 1
   }
   print(x)
