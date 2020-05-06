2b7edf26ed611b93776931096411638edf992af9
Joe Thorley <joe@poissonconsulting.ca>
2020-05-06 09:52:32 -0700

    - sbf_print() no longer silent by default.

--- R/print.R
+++ R/print.R
@@ -6,14 +6,16 @@
 #' 
 #' @param x An object to print.
 #' @param ntry A positive whole number specifying the number of attempts.
+#' @param silent A flag specifying whether to silence error messages.
 #'  
 #' @export
-sbf_print <- function(x, ntry = 3L) {
+sbf_print <- function(x, ntry = 3L, silent = FALSE) {
   chk_whole_number(ntry)
   chk_gt(ntry)
+  chk_flag(silent)
   i <- 1
   while(i < ntry) {
-    try <- try(print(x), silent = TRUE)
+    try <- try(print(x), silent = silent)
     if(!vld_is(try, "try-error"))
       return(invisible(try))
   }
