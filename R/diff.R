97f2e6966763b9c4b0688671c933f61de456bd14
Joe Thorley <joe@poissonconsulting.ca>
2020-10-14 17:56:30 -0700

    - Added exists = NA to sbf_diff_xx() functions and removed sbf_diff_plot_data().
    Not sure sbf_diff_plot_data() take plot (from window?) or data?

--- R/diff.R
+++ R/diff.R
@@ -1,59 +1,45 @@
 #' Diff Data
 #' 
 #' Find differences with existing data.
+#' If doesn't exist (exists = NA) x is compared to itself.
 #'
 #' @inheritParams sbf_save_object
 #' @inheritParams sbf_load_object
 #' @return A daff difference object.
 #' @export
 sbf_diff_data <- function(x, x_name = substitute(x), 
-                          sub = sbf_get_sub(), main = sbf_get_main()) {
+                          sub = sbf_get_sub(), main = sbf_get_main(), 
+                          exists = NA) {
   chk_s3_class(x, "data.frame")
   x_name <- chk_deparse(x_name)
   
   if(!requireNamespace("daff", quietly = TRUE))
     stop("Please `install.packages('daff')`.", call. = FALSE)
   
-  y <- sbf_load_data(x_name, sub = sub, main = main, exists = TRUE)
+  y <- sbf_load_data(x_name, sub = sub, main = main, exists = exists)
+  if(is.null(y)) y <- x
   daff::diff_data(y, x)
 }
 
 #' Diff Table
 #' 
 #' Find differences with existing table data.
-#'
+#' If doesn't exist (exists = NA) x is compared to itself.
+#' 
 #' @inheritParams sbf_save_object
 #' @inheritParams sbf_load_object
 #' @return A daff difference object.
 #' @export
 sbf_diff_table <- function(x, x_name = substitute(x), 
-                           sub = sbf_get_sub(), main = sbf_get_main()) {
+                           sub = sbf_get_sub(), main = sbf_get_main(), 
+                           exists = NA) {
   chk_s3_class(x, "data.frame")
   x_name <- chk_deparse(x_name)
   
   if(!requireNamespace("daff", quietly = TRUE))
     stop("Please `install.packages('daff')`.", call. = FALSE)
 
-  y <- sbf_load_table(x_name, sub = sub, main = main, exists = TRUE)
-  daff::diff_data(y, x)
-}
-
-#' Diff Plot Data
-#' 
-#' Find differences with existing plot data.
-#'
-#' @inheritParams sbf_save_object
-#' @inheritParams sbf_load_object
-#' @return A daff difference object.
-#' @export
-sbf_diff_plot_data <- function(x, x_name = substitute(x), 
-                               sub = sbf_get_sub(), main = sbf_get_main()) {
-  chk_s3_class(x, "data.frame")
-  x_name <- chk_deparse(x_name)
-
-  if(!requireNamespace("daff", quietly = TRUE))
-    stop("Please `install.packages('daff')`.", call. = FALSE)
-
-  y <- sbf_load_plot_data(x_name, sub = sub, main = main, exists = TRUE)
+  y <- sbf_load_table(x_name, sub = sub, main = main, exists = exists)
+  if(is.null(y)) y <- x
   daff::diff_data(y, x)
 }
--- tests/testthat/test-diff.R
+++ tests/testthat/test-diff.R
@@ -10,7 +10,8 @@ test_that("data",{
   expect_is(sbf_diff_data(x), "data_diff")
   
   y <- x
-  expect_error(sbf_diff_data(y))
+  expect_error(sbf_diff_data(y, exists = TRUE))
+  expect_is(sbf_diff_data(y), "data_diff")
   expect_is(sbf_diff_data(y, "x"), "data_diff")
   x <- 1
   expect_error(sbf_diff_data(x))
@@ -28,27 +29,9 @@ test_that("table",{
   expect_is(sbf_diff_table(x), "data_diff")
   
   y <- x
-  expect_error(sbf_diff_table(y))
+  expect_error(sbf_diff_table(y, exists = TRUE))
+  expect_is(sbf_diff_table(y), "data_diff")
   expect_is(sbf_diff_table(y, "x"), "data_diff")
   x <- 1
   expect_error(sbf_diff_table(x))
-})
-
-test_that("plot_data",{
-  teardown(sbf_reset_sub(rm = TRUE, ask = FALSE))
-  expect_identical(sbf_reset_sub(rm = TRUE, ask = FALSE), character(0))
-  
-  sbf_set_main(tempdir())
-  
-  x <- data.frame(x = 1, y = 2)
-  z <- ggplot2::ggplot(data = x, ggplot2::aes(x = x, y = y))
-  
-  expect_is(sbf_save_plot(z), "character")
-  expect_identical(sbf_load_plot_data("z"), x)
-  
-  expect_is(sbf_diff_plot_data(x, "z"), "data_diff")
-  
-  expect_error(sbf_diff_plot_data(x))
-  x <- 1
-  expect_error(sbf_diff_plot_data(x))
 })
