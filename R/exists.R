f37cd33797fecfd4d5276fd5b0bc5b410553bf83
Alan Constant <alan@poissonconsulting.ca>
2021-12-07 18:43:29 -0500

    deprecate sbf_object_exists - Deprecated `sbf_object_exists()`

--- R/exists.R
+++ R/exists.R
@@ -4,15 +4,17 @@ exists_rds <- function(x_name, class, sub, main) {
 }
 
 #' Object Exists
-#'
+#' @description this function is now deprecated as of version 0.0.0.9045
 #' @inheritParams sbf_save_data
 #' @return A flag specifying whether the object exists.
 #' @export
 sbf_object_exists <- function(x_name,
                               sub = sbf_get_sub(),
                               main = sbf_get_main()) {
+  lifecycle::deprecate_soft("0.0.0.9045", "sbf_object_exists()")
   exists_rds(x_name, class = "objects", sub = sub, main = main)
 }
+
 
 #' Data Exists
 #'
@@ -24,6 +26,7 @@ sbf_data_exists <- function(x_name,
                             main = sbf_get_main()) {
   exists_rds(x_name, class = "data", sub = sub, main = main)
 }
+
 
 #' Number Exists
 #'
