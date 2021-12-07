9caa11f54e52ca0c7b0c59ee5a8a563712ec6ddc
Alan Constant <alan@poissonconsulting.ca>
2021-12-07 18:51:21 -0500

    deprecated sbf_number_exists - Deprecated `sbf_number_exists()`

--- R/exists.R
+++ R/exists.R
@@ -30,13 +30,14 @@ sbf_data_exists <- function(x_name,
 
 
 #' Number Exists
-#'
+#' @description this function is now deprecated as of version 0.0.0.9045
 #' @inheritParams sbf_save_data
 #' @return A flag specifying whether the number exists.
 #' @export
 sbf_number_exists <- function(x_name,
                               sub = sbf_get_sub(),
                               main = sbf_get_main()) {
+  lifecycle::deprecate_soft("0.0.0.9045", "sbf_number_exists()")
   exists_rds(x_name, class = "numbers", sub = sub, main = main)
 }
 
