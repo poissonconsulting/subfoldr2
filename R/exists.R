fefabfa6f62127dcd4d44423f05b05e0c85c42f6
Alan Constant <alan@poissonconsulting.ca>
2021-12-07 19:04:43 -0500

    deprecated sbf_plot_exists, finished deprecating the sbf_*_exists family - Deprecated `sbf_plot_exists()`

--- R/exists.R
+++ R/exists.R
@@ -78,12 +78,13 @@ sbf_table_exists <- function(x_name,
 }
 
 #' Plot Exists
-#'
+#' @description this function is now deprecated as of version 0.0.0.9045
 #' @inheritParams sbf_save_object
 #' @return A flag specifying whether the plot exists.
 #' @export
 sbf_plot_exists <- function(x_name,
                             sub = sbf_get_sub(),
                             main = sbf_get_main()) {
+  lifecycle::deprecate_soft("0.0.0.9045", "sbf_plot_exists()")
   exists_rds(x_name, class = "plots", sub = sub, main = main)
 }
