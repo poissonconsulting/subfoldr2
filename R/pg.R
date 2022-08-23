5c8fac571da782d02726e748e23ec9e4f89a03d0
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-23 13:44:16 -0700

    adding load table and adding postgresql family

--- R/pg.R
+++ R/pg.R
@@ -6,6 +6,7 @@
 #' @return
 #' @export
 #' @details Wrapper on `psql::psql_connect()`
+#' @family postgresql functions
 #'
 #' @examples
 #' \dontrun{
@@ -37,6 +38,7 @@ sbf_open_pg <- function(config_path = getOption("psql.config_path", NULL),
 #' @export
 #' @details Wrapper on `DBI::dbDisconnect()`. It is important to remember to 
 #' close connections or your database performance can decrease over time. 
+#' @family postgresql functions
 #'
 #' @examples
 #' \dontrun{
@@ -58,6 +60,7 @@ sbf_close_pg <- function(conn) {
 #' @return TRUE (or errors)
 #' @export
 #' @details Wrapper on `psql::psql_backup()`
+#' @family postgresql functions
 #'
 #' @examples
 #' \dontrun{
@@ -84,6 +87,7 @@ sbf_save_pg <- function(
 #' @return TRUE (or errors).
 #' @export
 #' @details Wrapper on `psql::psql_create_db()`
+#' @family postgresql functions
 #'
 #' @examples
 sbf_create_pg <- function(
@@ -107,6 +111,7 @@ sbf_create_pg <- function(
 #' @return A scalar numeric of the number of rows affected by the statement.
 #' @export
 #' @details Wrapper on `psql::psql_execute_db()`
+#' @family postgresql functions
 #'
 #' @examples
 #' \dontrun{
@@ -140,6 +145,7 @@ sbf_execute_pg <- function(
 #' @return A vector of table names
 #' @export
 #' @details Wrapper on `psql::psql_list_tables()`
+#' @family postgresql functions
 #'
 #' @examples
 #' \dontrun{
@@ -163,6 +169,36 @@ sbf_list_tables_pg <- function(
 
 
 
+#' Load a table from a PostgreSQL database
+#'
+#' Read/load a table from a PostgreSQL database as a data frame into R.
+#'
+#' @inheritParams psql::psql_read_table
+#'
+#' @return A data frame
+#' @export
+#' @details Wrapper on `psql::psql_read_table()`
+#' @family postgresql functions
+#'
+#' @examples
+#' \dontrun{
+#' sbf_load_data_from_pg("capture")
+#' sbf_load_data_from_pg("counts", "boat_count")
+#' }
+sbf_load_data_from_pg <- function(
+    tbl_name,
+    schema = "public",
+    config_path = getOption("psql.config_path", NULL),
+    config_value = getOption("psql.value", NULL)
+  ) {
+  psql::psql_read_table(
+    tbl_name = tbl_name,
+    schema = schema,
+    config_path = config_path,
+    config_value = config_value
+  )
+}
+
 #' Load Data Frames from PostgreSQL Database
 #' 
 #' Load all the tables in a schema as data frames into your environment from a 
@@ -171,14 +207,21 @@ sbf_list_tables_pg <- function(
 #' @inheritParams sbf_load_objects
 #' @inheritParams psql::psql_list_tables
 #' @return An invisible character vector of the paths to the saved objects.
+#' @export
 #' @family load functions
 #' @family postgresql functions
-#' @export
-sbf_load_datas_from_psql <- function(schema = "public",
-                                     rename = identity,
-                                     env = parent.frame(),
-                                     config_path = getOption("psql.config_path", NULL),
-                                     config_value = getOption("psql.value", NULL)) {
+#' 
+#' @examples
+#' \dontrun{
+#' sbf_load_datas_from_pg()
+#' sbf_load_datas_from_pg(schema = "capture")
+#' sbf_load_datas_from_pg(rename = toupper)
+#' }
+sbf_load_datas_from_pg <- function(schema = "public",
+                                   rename = identity,
+                                   env = parent.frame(),
+                                   config_path = getOption("psql.config_path", NULL),
+                                   config_value = getOption("psql.value", NULL)) {
   chk_s3_class(env, "environment")
   chk_function(rename)
   
@@ -202,10 +245,3 @@ sbf_load_datas_from_psql <- function(schema = "public",
   mapply(assign, names(datas), datas, MoreArgs = list(envir = env))
   invisible(names(datas))
 }
-
-
-
-
-
-
-
