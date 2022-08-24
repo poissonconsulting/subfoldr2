b201975872a51dc6444cb531e82f1bf589a19331
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-23 17:42:51 -0700

    adding tests for postgresql functions

--- tests/testthat/test-pg.R
+++ tests/testthat/test-pg.R
@@ -151,3 +151,115 @@ test_that("reset config file value", {
   set_value <- sbf_get_config_value()
   expect_equal(set_value, character(0))
 })
+
+test_that("test sbf_open_pg works", {
+  skip_on_ci()
+  conn <- sbf_open_pg(config_path = NULL, config_value = NULL)
+  withr::defer(DBI::dbDisconnect(conn))
+  expect_s4_class(conn, "PqConnection")
+})
+
+test_that("test sbf_close_pg works", {
+  skip_on_ci()
+  conn <- sbf_open_pg(config_path = NULL, config_value = NULL)
+  withr::defer(
+    suppressWarnings(
+      DBI::dbDisconnect(conn)
+    )
+  )
+  sbf_close_pg(conn)
+  expect_error(
+    DBI::dbExecute(conn, "SELECT 1+1;"),
+    regexp = "external pointer is not valid"
+  )
+})
+
+test_that("test sbf_save_pg works", {
+  skip_on_ci()
+  config_path <- system.file("testhelpers/config-hosted.yml", package = "psql")
+  temp_dir <- withr::local_tempdir()
+  dump_path <- file.path(temp_dir, "dump_db1.sql")
+  sbf_save_pg(
+    path = dump_path,
+    config_path = config_path
+  )
+  expect_true(file.exists(dump_path))
+})
+
+test_that("test sbf_create_pg works", {
+  skip_on_ci()
+  output <- sbf_create_pg("newdb", NULL, NULL)
+  # clean up afterwards
+  withr::defer({
+    try(
+      result <- DBI::dbSendQuery(psql::psql_connect(NULL, NULL), "DROP DATABASE newdb;"),
+      silent = TRUE
+    )
+    try(
+      DBI::dbClearResult(result),
+      silent = TRUE
+    )
+  })
+  # test
+  expect_true(output)
+})
+
+test_that("test sbf_execute_pg works", {
+  skip_on_ci()
+  config_path <- system.file("testhelpers/config.yml", package = "psql")
+  output <- sbf_execute_pg(
+    "CREATE SCHEMA boat_count",
+    config_path = config_path
+  )
+  withr::defer({
+    try(
+      sbf_execute_pg(
+        "DROP SCHEMA boat_count",
+        config_path = config_path
+      ),
+      silent = TRUE
+    )
+  })
+  query <- DBI::dbGetQuery(
+    psql::psql_connect(config_path = config_path),
+    "SELECT schema_name FROM information_schema.schemata"
+  )
+  expect_equal(output, 0L)
+  expect_true("boat_count" %in% query$schema_name)
+})
+
+test_that("test sbf_list_tables_pg works", {
+  skip_on_ci()
+  # set up test
+  config_path <- system.file("testhelpers/config.yml", package = "psql")
+  sbf_execute_pg("CREATE SCHEMA boat_count", config_path = config_path)
+  withr::defer(
+    try(
+      psql::psql_execute_db(
+        "DROP SCHEMA boat_count",
+        config_path = config_path
+      )
+    )
+  )
+  sbf_execute_pg(
+    "CREATE TABLE boat_count.counts (
+     x INTEGER NOT NULL,
+     y INTEGER)",
+    config_path = config_path
+  )
+  withr::defer(
+    try(
+      psql::psql_execute_db(
+        "DROP TABLE boat_count.counts",
+        config_path = config_path
+      )
+    )
+  )
+  # execute tests
+  output <- sbf_list_tables_pg(
+    "boat_count",
+    config_path = config_path
+  )
+  expect_equal(output, c("counts"))
+  
+})
