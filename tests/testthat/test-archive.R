ad0f89be0610da6263b5600f947ceae41417f5c9
Ayla Pearson <ayla@poissonconsulting.ca>
2024-07-16 17:21:01 -0700

    trying to determine why the message is wrong on windows

--- tests/testthat/test-archive.R
+++ tests/testthat/test-archive.R
@@ -1,18 +1,18 @@
-test_that("archive", {
-  sbf_reset()
-  sbf_set_main(file.path(withr::local_tempdir(), "output"))
-  withr::defer(sbf_reset())
-
-  expect_chk_error(sbf_archive_main(ask = FALSE))
-
-  x <- 1
-  sbf_save_number(x)
-  expect_identical(sbf_load_number("x"), x)
-  expect_message(new_main <- sbf_archive_main(ask = FALSE))
-  expect_identical(sbf_load_number("x"), x)
-  expect_identical(sbf_load_number("x", main = new_main), x)
-  expect_true(grepl("\\d{4,4}(-\\d{2,2}){5,5}$", new_main))
-})
+# test_that("archive", {
+#   sbf_reset()
+#   sbf_set_main(file.path(withr::local_tempdir(), "output"))
+#   withr::defer(sbf_reset())
+# 
+#   expect_chk_error(sbf_archive_main(ask = FALSE))
+# 
+#   x <- 1
+#   sbf_save_number(x)
+#   expect_identical(sbf_load_number("x"), x)
+#   expect_message(new_main <- sbf_archive_main(ask = FALSE))
+#   expect_identical(sbf_load_number("x"), x)
+#   expect_identical(sbf_load_number("x", main = new_main), x)
+#   expect_true(grepl("\\d{4,4}(-\\d{2,2}){5,5}$", new_main))
+# })
 
 test_that("archive message", {
   sbf_reset()
@@ -27,45 +27,58 @@ test_that("archive message", {
   )
 })
 
-test_that("get_archive", {
+test_that("test2", {
   sbf_reset()
   sbf_set_main(file.path(withr::local_tempdir(), "output"))
   withr::defer(sbf_reset())
-
+  
   x <- 1
   sbf_save_number(x)
-  expect_message(new_main1 <- sbf_archive_main(ask = FALSE))
-  x <- 2
-  sbf_save_number(x)
-  expect_message(new_main2 <- sbf_archive_main(ask = FALSE))
-
-  expect_equal(as.character(sbf_get_archive(archive = 2L)), new_main1)
-  expect_equal(as.character(sbf_get_archive(archive = 1L)), new_main2)
-  expect_error(sbf_get_archive(archive = 3L))
+  expect_message(
+    sbf_archive_main(ask = FALSE),
+    paste0("Directory '", sbf_get_main(), "' copied to '", sbf_get_main())
+  )
 })
 
-test_that("unarchive", {
-  sbf_reset()
-  sbf_set_main(file.path(withr::local_tempdir(), "output"))
-  withr::defer(sbf_reset())
-
-  x <- 1
-  sbf_save_number(x)
-  expect_message(archive1 <- sbf_archive_main(ask = FALSE))
-  x <- 2
-  sbf_save_number(x)
-  expect_message(archive2 <- sbf_archive_main(ask = FALSE))
-  x <- 3
-  sbf_save_number(x)
-  expect_message(archive3 <- sbf_archive_main(ask = FALSE))
-
-  expect_identical(sbf_load_number("x"), 3)
-  expect_message(expect_message(expect_equal(sbf_unarchive_main(archive = archive1, ask = FALSE), archive1)))
-  expect_identical(sbf_load_number("x"), 1)
-  expect_message(expect_message(expect_equal(as.character(sbf_unarchive_main(ask = FALSE)), archive3)))
-  expect_identical(sbf_load_number("x"), 3)
-  expect_error(sbf_unarchive_main(archive = 2L, ask = FALSE))
-  expect_message(expect_message(expect_equal(as.character(sbf_unarchive_main(ask = FALSE)), archive2)))
-  expect_identical(sbf_load_number("x"), 2)
-  expect_error(sbf_unarchive_main(ask = FALSE))
-})
+# test_that("get_archive", {
+#   sbf_reset()
+#   sbf_set_main(file.path(withr::local_tempdir(), "output"))
+#   withr::defer(sbf_reset())
+# 
+#   x <- 1
+#   sbf_save_number(x)
+#   expect_message(new_main1 <- sbf_archive_main(ask = FALSE))
+#   x <- 2
+#   sbf_save_number(x)
+#   expect_message(new_main2 <- sbf_archive_main(ask = FALSE))
+# 
+#   expect_equal(as.character(sbf_get_archive(archive = 2L)), new_main1)
+#   expect_equal(as.character(sbf_get_archive(archive = 1L)), new_main2)
+#   expect_error(sbf_get_archive(archive = 3L))
+# })
+# 
+# test_that("unarchive", {
+#   sbf_reset()
+#   sbf_set_main(file.path(withr::local_tempdir(), "output"))
+#   withr::defer(sbf_reset())
+# 
+#   x <- 1
+#   sbf_save_number(x)
+#   expect_message(archive1 <- sbf_archive_main(ask = FALSE))
+#   x <- 2
+#   sbf_save_number(x)
+#   expect_message(archive2 <- sbf_archive_main(ask = FALSE))
+#   x <- 3
+#   sbf_save_number(x)
+#   expect_message(archive3 <- sbf_archive_main(ask = FALSE))
+# 
+#   expect_identical(sbf_load_number("x"), 3)
+#   expect_message(expect_message(expect_equal(sbf_unarchive_main(archive = archive1, ask = FALSE), archive1)))
+#   expect_identical(sbf_load_number("x"), 1)
+#   expect_message(expect_message(expect_equal(as.character(sbf_unarchive_main(ask = FALSE)), archive3)))
+#   expect_identical(sbf_load_number("x"), 3)
+#   expect_error(sbf_unarchive_main(archive = 2L, ask = FALSE))
+#   expect_message(expect_message(expect_equal(as.character(sbf_unarchive_main(ask = FALSE)), archive2)))
+#   expect_identical(sbf_load_number("x"), 2)
+#   expect_error(sbf_unarchive_main(ask = FALSE))
+# })
